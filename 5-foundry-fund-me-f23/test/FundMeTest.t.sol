14ceda7ad7add9627b9930541b084aebe795069e
ShisukeUrahara <abhisheksagu@gmail.com>
2024-05-26 12:53:29 +0530

    5-foundry-fund-me-f23: Refactored code

--- 5-foundry-fund-me-f23/test/FundMeTest.t.sol
+++ 5-foundry-fund-me-f23/test/FundMeTest.t.sol
@@ -7,7 +7,7 @@ import {PriceConverter} from "../src/PriceConverter.sol";
 contract FundMeTest is Test {
     FundMe fundMe;
     function setUp() external {
-        fundMe = new FundMe();
+        fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306); // aggregator price feed for sepolia
     }
 
     function testMinUSD() public {
@@ -16,5 +16,10 @@ contract FundMeTest is Test {
 
     function testOwnerIsMsgSender() public {
         assertEq(fundMe.i_owner(), address(this));
+    }
+
+    function testCorrectPriceFeedVersion() public {
+        uint256 version = fundMe.getVersion();
+        assertEq(version, 4);
     }
 }
