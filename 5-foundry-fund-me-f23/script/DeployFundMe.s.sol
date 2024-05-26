349647356b168ac90443cf0a8a933a122346fa39
ShisukeUrahara <abhisheksagu@gmail.com>
2024-05-26 12:34:21 +0530

    5-Foundry-Fund-Me-f23: Added basic deploy script

---
+++ 5-foundry-fund-me-f23/script/DeployFundMe.s.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+import {Script} from "forge-std/Script.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract DeployFundMe is Script {
+    function run() external {
+        vm.startBroadcast();
+        FundMe fundMe = new FundMe();
+        vm.stopBroadcast();
+    }
+}
