e167d38b5463fca6cea79e47e95f9081fb00f5a5
ShisukeUrahara <abhisheksagu@gmail.com>
2024-08-03 19:03:18 +0530

    Added scripts for adding newly deployed raffle contract as a consumer for the vrf coordinator

--- 6-foundry-smart-contract-lottery-f23/script/DeployRaffle.s.sol
+++ 6-foundry-smart-contract-lottery-f23/script/DeployRaffle.s.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import {Raffle} from "../src/Raffle.sol";
 import {Script} from "forge-std/Script.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
-import {CreateSubscription} from "./Interactions.s.sol";
+import {CreateSubscription, FundSubscription, AddConsumer} from "./Interactions.s.sol";
 
 contract DeployRaffle is Script {
     function run() external returns (Raffle, HelperConfig) {
@@ -27,6 +27,12 @@ contract DeployRaffle is Script {
             );
 
             // fund the subscription
+            FundSubscription fundSubscription = new FundSubscription();
+            fundSubscription.fundSubscription(
+                vrfCoordinator,
+                subscriptionId,
+                linkTokenAddress
+            );
         }
 
         vm.startBroadcast();
@@ -40,6 +46,14 @@ contract DeployRaffle is Script {
         );
 
         vm.stopBroadcast();
+
+        // adding the raffle as the consumer contract in the subscription
+        AddConsumer addConsumer = new AddConsumer();
+        addConsumer.addConsumer(
+            address(raffle),
+            vrfCoordinator,
+            subscriptionId
+        );
         return (raffle, helperConfig);
     }
 }
--- 6-foundry-smart-contract-lottery-f23/script/Interactions.s.sol
+++ 6-foundry-smart-contract-lottery-f23/script/Interactions.s.sol
@@ -6,6 +6,7 @@ import {Script, console} from "forge-std/Script.sol";
 import {VRFCoordinatorV2_5Mock} from "chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
 import {LinkToken} from "../test/mocks/LinkToken.sol";
+import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
 
 contract CreateSubscription is Script {
     function createSubscriptionUsingChainConfig() public returns (uint256) {
@@ -87,5 +88,46 @@ contract FundSubscription is Script {
 
     function run() external {
         fundSubscriptionUsingConfig();
+    }
+}
+
+contract AddConsumer is Script {
+    function addConsumerUsingConfig(address raffle) public {
+        HelperConfig helperConfig = new HelperConfig();
+        (
+            ,
+            ,
+            address vrfCoordinator,
+            ,
+            uint256 subId,
+            ,
+            address linkTokenAddress
+        ) = helperConfig.activeNetworkConfig();
+
+        addConsumer(raffle, vrfCoordinator, subId);
+    }
+
+    function addConsumer(
+        address raffle,
+        address vrfCoordinator,
+        uint256 subId
+    ) public {
+        console.log("**@ adding consumer contract , ", raffle);
+        console.log("**@ using coordinator , ", vrfCoordinator);
+        console.log("on chainId , ", block.chainid);
+
+        vm.startBroadcast();
+        VRFCoordinatorV2_5Mock(vrfCoordinator).addConsumer(subId, raffle);
+
+        vm.stopBroadcast();
+    }
+
+    function run() external {
+        address raffle = DevOpsTools.get_most_recent_deployment(
+            "Raffle",
+            block.chainid
+        );
+
+        addConsumerUsingConfig(raffle);
     }
 }
