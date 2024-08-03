ac0a5834a6beac2e4a9fa01a9715b2ae92926a80
ShisukeUrahara <abhisheksagu@gmail.com>
2024-08-03 12:59:49 +0530

    6-foundry-smart-contract-lottery: Updated Raffle deploy script

--- 6-foundry-smart-contract-lottery-f23/script/DeployRaffle.s.sol
+++ 6-foundry-smart-contract-lottery-f23/script/DeployRaffle.s.sol
@@ -3,5 +3,31 @@ pragma solidity ^0.8.20;
 
 import {Raffle} from "../src/Raffle.sol";
 import {Script} from "forge-std/Script.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
-contract DeployRaffle is Script {}
+contract DeployRaffle is Script {
+    function run() external returns (Raffle) {
+        HelperConfig helperConfig = new HelperConfig();
+        (
+            uint256 entranceFees,
+            uint256 interval,
+            address vrfCoordinator,
+            bytes32 gasLane,
+            uint64 subscriptionId,
+            uint32 callbackGasLimit
+        ) = helperConfig.activeNetworkConfig();
+
+        vm.startBroadcast();
+        Raffle raffle = new Raffle(
+            entranceFees,
+            interval,
+            vrfCoordinator,
+            gasLane,
+            subscriptionId,
+            callbackGasLimit
+        );
+
+        vm.stopBroadcast();
+        return raffle;
+    }
+}
